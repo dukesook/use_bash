4699e619d39716dd6f3c5c40f8bc0df828a9667e
dukesook <devonsookhoo14@gmail.com>
2026-03-13 21:32:23 -0600

    {} replace text inside a variable

--- parameter_expansion/parameter_expansion_answers.sh
+++ parameter_expansion/parameter_expansion_answers.sh
@@ -1,10 +1,14 @@
+x="file.txt"
+path="/home/user/photo.jpg"
+
 # Question 1 - Remove the .txt file extension
 
-x="file.txt"
 echo ${x%.txt}
 
 # Question 2 - Remove the directory from a path
 
-path="/home/user/photo.jpg"
 echo "${path##*/}"
 basename "$path"
+
+# Question 3 - Replace Text inside of a variable
+echo ${x/file/doc}
